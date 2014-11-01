30cb3195c9c311661bf2cbe512f1a09dac9ff5f3
oldgeezr <ramstadevensen@hotmail.com>
2014-11-01 17:31:36 +0100

    the filler

---
+++ filler.v
@@ -0,0 +1,45 @@
+module filler
+#(
+  parameter ADDR_WIDTH = 15 // Video format: QQVGA = 160x120 => 2^15
+)
+(
+  // Input
+  input                       reset_n,
+  input                       clk_25,
+  output reg                  we, // temp
+  output reg [ADDR_WIDTH-1:0] write_addr, // temp
+  output reg                  pixel // temp
+);
+  // temp
+  reg [7:0] h_count;
+  reg [6:0] v_count;
+
+  always @ (posedge clk_25 or negedge reset_n) begin
+    if (~reset_n) begin
+      h_count <= 0;
+      v_count <= 0;
+      write_addr <= 0;
+      we <= 1;
+    else begin
+      pixel <= 1;
+
+      if ((h_count > 40 && h_count < 119) && (v_count > 30 && v_count < 89))
+        pixel <= 0;
+
+      if (h_count < 159)
+        h_count <= h_count + 1;
+      else begin
+        h_count <= 0;
+        if (v_count < 119)
+          v_count <= v_count + 1;
+        else
+          v_count <= 0;
+      end
+
+      if (write_addr < 19199)
+        write_addr <= write_addr + 1;
+      else
+        write_addr <= 0;
+    end
+  end
+endmodule
---
+++ filler_tb.sv
@@ -0,0 +1,36 @@
+module filler_tb
+#(
+  parameter ADDR_WIDTH = 15 // Video format: QQVGA = 160x120 => 2^15
+)
+
+  // Input
+  reg                    reset_n,
+  reg                    clk_25,
+
+  wire [(ADDR_WIDTH-1):0] write_addr;
+  wire                    we;
+  wire                    pixel;
+
+  initial begin
+    clk_25 = 0;
+    reset_n = 1;
+
+    #5 reset_n = 0;
+
+    #10 reset_n = 1;
+  end
+
+
+  always #5 clk_25 = ~clk_25;
+
+
+  filler fill_FB
+  (
+    .clk_25(clk_25),
+    .reset_n(reset_n)
+    .we(we),
+    .write_addr(write_addr),
+    .pixel(pixel)
+  );
+
+endmodule
